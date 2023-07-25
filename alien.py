5e76304cbc8d0f8d991b0d1c43cf3b7d9cd907cf
bretthinkle <130404904+bretthinkle@users.noreply.github.com>
2023-07-25 02:12:49 -0400

    Finished alien.py

--- alien.py
+++ alien.py
@@ -5,11 +5,12 @@ class Alien(Sprite):
     """A class which represents a single alien in the fleet"""
 
     def __init__(self, ai_game):
-        super.__init()
-        self.screen = ai_game
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load image of alien and set its rect attribute
-        self.image = self.image.load('alien.bmp')
+        self.image = pygame.image.load('alien.bmp')
         self.rect = self.image.get_rect()
 
         # Set location of alien
@@ -18,3 +19,14 @@ class Alien(Sprite):
 
         # Store aliens horizontal position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right"""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Return True if aline is at edge of screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
